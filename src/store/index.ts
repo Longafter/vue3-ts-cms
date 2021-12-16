64b883405e161f96c616faaab6a56fb5f0bc635c
zhangpingzhao <0049003615@znv.com>
2021-12-16 14:41:02 +0800

    vuex集成

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'pingzhao'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+
+export default store

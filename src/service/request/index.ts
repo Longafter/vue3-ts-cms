dc45b87d597bbe1f2481cbcbe7c426fdc9b2cb70
zhangpingzhao <0049003615@znv.com>
2021-12-17 14:08:18 +0800

    PZRequest的请求config拦截器的封装

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,14 +1,27 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance } from 'axios'
+import type { PZRequestInterceptors, PZRequestConfig } from './type'
 
 class PZRequest {
   // 传入不同配置时，每次都会创建新的instance
   instance: AxiosInstance
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: PZRequestInterceptors
+
+  constructor(config: PZRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
 
-  request(config: AxiosRequestConfig): void {
+  request(config: PZRequestConfig): void {
     this.instance.request(config).then(res => {
       console.log(res)
     })
